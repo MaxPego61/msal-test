dd608994cae3ea3232d0a8c8848a65fe4b3df816
MaxPego61 <pegoraro.massimo@gmail.com>
2026-06-02 19:10:28 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { useMsal, useIsAuthenticated } from "@azure/msal-react";
+import { loginRequest } from "./msalConfig";
+
+function App() {
+  const { instance } = useMsal();
+  const isAuthenticated = useIsAuthenticated();
+
+  const login = () => {
+    instance.loginPopup(loginRequest);
+  };
+
+  const logout = () => {
+    instance.logoutPopup();
+  };
+
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>MSAL Test Login</h1>
+
+      {!isAuthenticated && (
+        <button onClick={login}>Login con Microsoft</button>
+      )}
+
+      {isAuthenticated && (
+        <>
+          <p>Login effettuato!</p>
+          <button onClick={logout}>Logout</button>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
